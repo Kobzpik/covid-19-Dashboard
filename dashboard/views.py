21a3a6cae064a402cbfee22d4ea9bb3c3a8c2a5d
Kobzpik <prabath.pik2299@gmail.com>
2021-12-09 18:04:13 +0530

    add chart

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
+import pandas as pd
+
+
 
 # Create your views here.
 
 def home(request):
-        return render(request,'index.htm')
+        
+        confirmedGlobal=pd.read_csv('https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv',encoding='utf-8',na_values=None)
+        totalCount=confirmedGlobal[confirmedGlobal.columns[5]].sum()
+        bardata=confirmedGlobal[['Country/Region',confirmedGlobal.columns[5]]].groupby('Country/Region').sum()
+        bardata=bardata.reset_index()
+        bardata.columns=['Country/Region','values']
+        bardata=bardata.sort_values(by='values',ascending=False)
+        countryName=bardata['Country/Region'].values.tolist()
+        barVal=bardata['values'].values.tolist()
+        context={'totalCount':totalCount,'countryName':countryName,'barVal':barVal}
+        return render(request,'index.htm', context)
